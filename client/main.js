bd16cebb6e9a25d97eb701a2df9f71e3650481de
julian <jmendiola@atixlabs.com>
2016-04-11 16:33:11 -0300

    adding some bootstrap

--- client/main.js
+++ client/main.js
@@ -1,19 +1,4 @@
-import { Template } from 'meteor/templating'
-import { ReactiveVar } from 'meteor/reactive-var'
-import { ReactiveDict } from 'meteor/reactive-dict'
+
 
 import '../imports/startup/client/index.js'
-import { Invoices } from '../imports/api/invoices.js'
 
-import './main.html'
-
-Template.body.onCreated(function bodyOnCreated() {
-  this.state = new ReactiveDict();
-  Meteor.subscribe('invoices');
-});
-
-Template.body.helpers({
-  invoices() {
-    return Invoices.find({});
-  }
-})
--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,5 +1,23 @@
 import { Template } from 'meteor/templating'
+import { ReactiveVar } from 'meteor/reactive-var'
+import { ReactiveDict } from 'meteor/reactive-dict'
+
+import { Invoices } from '../../api/invoices.js'
+
+import '../../ui/pages/invoices.html'
+import '../../ui/layouts/main.css'
+
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+    Meteor.subscribe('invoices');
+});
 
 Template.registerHelper('formatDate', function(date) {
     return moment(date).format('YYYY-MM-DD');
 });
+
+Template.body.helpers({
+    invoices() {
+        return Invoices.find({});
+    }
+})
